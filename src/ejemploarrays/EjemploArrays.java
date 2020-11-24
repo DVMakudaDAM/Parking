53a6ff6de56164b184911da505a190f6ff9f1799
dam <dam@172.26.39.99>
2020-11-24 11:51:57 +0100

    version1

---
+++ src/ejemploarrays/EjemploArrays.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejemploarrays;
+
+/**
+ *
+ * @author dam
+ */
+public class EjemploArrays {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       /* Aula miAula;
+        miAula = new Aula();
+        /*Aula miAula2;
+        miAula2 = new Aula(2);*/
+        
+       /* miAula.cargarNotas();*/
+       
+       Aula2 miAula;
+       miAula = new Aula2();
+       miAula = new Aula2(5);
+    }
+    
+       
+    
+}
